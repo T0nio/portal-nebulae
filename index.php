44dfcb753cc86e6c1a00868963c85932244288e0
Antoine <main@toni0.com>
2017-05-04 18:16:29 -0700

    Init Bootstrap

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>The Nebulae - Portail</title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="/css/bootstrap.min.js" crossorigin="anonymous">
+
+
+	</head>
+	<body>
+		<h1 class="text-center">Bienvenue</h1>
+		<h2 class="text-center">Sur la première version du portail Nebulae</h2>
+
+		<!-- jQuery -->
+		<script src="/js/jquery.min.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="/js/boostrap.min.js"></script>
+	</body>
+</html>
